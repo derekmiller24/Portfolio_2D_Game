743c2f00cceef55e84d67c860c63103c370a55ed
derekmiller24 <derek.miller826@gmail.com>
2024-12-23 15:48:38 -0600

    New account/repo

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in .NET/Python these days.
+    I've upgraded websites and built business applications. I also like Javascript and SQL. I find learning new languages fun!
+    Here is my <a href="https://github.com/derekmiller88" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Info Sys degree. I graduated in 2014, but I'm still learning new things every day!`,
+  "sofa-table": `That's my sofa. I like to relax here and watch programming tutorials on YouTube. 
+  I also watch indoor growing tutorials for my hemp business!`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@JSLegendDev" target="_blank">JSLegendDev</a>, <a href="https://www.youtube.com/@MrCanucksGrowGuide" target="_blank">Mr Canucks Grow</a>`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/derekmiller88/Resume/blob/main/blob/main/DerekResume3.2.pdf" target="_blank">Check it out?</a>
+  Contact me at derek.miller826@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of JSLegendDV's tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `There are a variety of books on my bookshelf. I like to read books on programming, business, and self-improvement.`,
+  exit: `If you want to exit Derek's portfolio, just close the tab.`,
+};
